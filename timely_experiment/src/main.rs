bd111ea08502764d4571ccc47ecb88b10a94271b
Murray Dunne <mrmurraydunne@gmail.com>
2019-11-18 16:45:28 -0500

    Fixed gitignored. Added simple timely demo.

---
+++ timely_experiment/src/main.rs
@@ -0,0 +1,24 @@
+use timely::*;
+use timely::dataflow::operators::{Input, Inspect};
+
+fn main() {
+
+    // construct and execute a timely dataflow
+    timely::execute_from_args(std::env::args(), |worker| {
+
+        // add an input and base computation off of it
+        let mut input = worker.dataflow(|scope| {
+            let (input, stream) = scope.new_input();
+            stream.inspect(|x| println!("hello {:?}", x));
+            input
+        });
+
+        // introduce input, advance computation
+        for round in 0..10 {
+            input.send(round);
+            input.advance_to(round + 1);
+            worker.step();
+        }
+    });
+
+}

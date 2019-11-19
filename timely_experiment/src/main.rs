0745ce12a9b8fd03f523514ee67609be2e655ad1
Murray Dunne <mrmurraydunne@gmail.com>
2019-11-19 16:33:54 -0500

    Got frames processing out of order.

--- timely_experiment/src/main.rs
+++ timely_experiment/src/main.rs
@@ -4,7 +4,6 @@ use timely::dataflow::channels::pact::Pipeline;
 fn main() {
 
     // construct and execute a timely dataflow
-    #[allow(unused_must_use)]
     timely::execute_from_args(std::env::args(), |worker| {
 
         // add an input and base computation off of it
@@ -12,32 +11,34 @@ fn main() {
             let index = scope.index();
             let (input, stream) = scope.new_input();
             stream.unary(Pipeline, "DropNotNeeded", |_, _| {
-                        let mut vector = Vec::new();
+                        let mut vector : Vec<(u64, u64)> = Vec::new();
                         move |input, output| {
                             while let Some((time, data)) = input.next() {
                                 data.swap(&mut vector);
                                 let mut session = output.session(&time);
                                 for datum in vector.drain(..) {
-                                    if datum == index as u64 {
+                                    if datum.0 == index as u64 {
                                         session.give(datum);
                                     }
                                 }
                             }
                         }
                     })
-                    .exchange(|x| *x % 5 as u64)
-                    .map(|x| x * 2)
-                    .inspect(move |x| println!("{:?} did {:?} - first", index, x / 2))
-                    .exchange(|x| (*x / 2) + 5 as u64)
-                    .map(|x| x * 3)
-                    .inspect(move |x| println!("{:?} did {:?} - second", index, x / 6));
+                    .exchange(|x: &(u64, u64)| (*x).1)
+                    //.map(|x| x * 2)
+                    .inspect(move |x: &(u64, u64)| println!("node {:?} pipeline {:?} frame {:?} - first", index, (*x).1, (*x).0))
+                    .exchange(|x: &(u64, u64)| (*x).1 + 5)
+                    //.map(|x| x * 3)
+                    .inspect(move |x: &(u64, u64)| println!("node {:?} pipeline {:?} frame {:?} - second", index, (*x).1, (*x).0));
             input
         });
 
         // introduce input, advance computation
-        for round in 0..5 {
-            input.send(round);
-            input.advance_to(round + 1);
+        for video_frame_index in 0..23 {
+            for pipeline in 0..5 {
+                input.send((video_frame_index, pipeline));
+            }
+            input.advance_to(video_frame_index + 1);
             worker.step();
         }
     }).ok();

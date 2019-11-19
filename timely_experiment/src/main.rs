9962956755bc6f87a7d9bc1c144b30393c33a8f2
Murray Dunne <mrmurraydunne@gmail.com>
2019-11-19 15:25:38 -0500

    Fixed node assignments. Cleaned up warnings.

--- timely_experiment/src/main.rs
+++ timely_experiment/src/main.rs
@@ -1,18 +1,17 @@
-use timely::*;
 use timely::dataflow::operators::*;
-use timely::dataflow::operators::generic::*;
 use timely::dataflow::channels::pact::Pipeline;
 
 fn main() {
 
     // construct and execute a timely dataflow
+    #[allow(unused_must_use)]
     timely::execute_from_args(std::env::args(), |worker| {
 
         // add an input and base computation off of it
         let mut input = worker.dataflow(|scope| {
             let index = scope.index();
             let (input, stream) = scope.new_input();
-            stream.unary(Pipeline, "DropNotNeeded", |capability, info| {
+            stream.unary(Pipeline, "DropNotNeeded", |_, _| {
                         let mut vector = Vec::new();
                         move |input, output| {
                             while let Some((time, data)) = input.next() {
@@ -29,7 +28,7 @@ fn main() {
                     .exchange(|x| *x % 5 as u64)
                     .map(|x| x * 2)
                     .inspect(move |x| println!("{:?} did {:?} - first", index, x / 2))
-                    .exchange(|x| *x % 5 + 5 as u64)
+                    .exchange(|x| (*x / 2) + 5 as u64)
                     .map(|x| x * 3)
                     .inspect(move |x| println!("{:?} did {:?} - second", index, x / 6));
             input
@@ -41,5 +40,5 @@ fn main() {
             input.advance_to(round + 1);
             worker.step();
         }
-    });
+    }).ok();
 }

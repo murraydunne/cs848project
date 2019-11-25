27754a2103f24b2e9cbdadadc5311427f90bf126
Murray Dunne <mrmurraydunne@gmail.com>
2019-11-25 17:20:02 -0500

    We have a pipeline!

--- timely_experiment/src/main.rs
+++ timely_experiment/src/main.rs
@@ -3,25 +3,18 @@ use timely::dataflow::channels::pact::Pipeline;
 use std::{thread, time};
 use pyo3::{prelude::*, types::{IntoPyDict, PyModule, PyByteArray}};
 use std::boxed::Box;
+use opencv::prelude::*;
+use opencv::core;
+use opencv::videoio;
+use opencv::highgui;
+use std::ptr;
+
 
 #[macro_use]
 extern crate lazy_static;
 
 
-fn execute_python_module(param: u64, worker: u64, frame: &Vec<u8>) -> f64 {
-    println!("{:?} - Hello from the function!", worker);
-
-    /*
-    lazy_static! {
-        static ref HASHMAP: HashMap<u32, &'static str> = {
-            let mut m = HashMap::new();
-            m.insert(0, "foo");
-            m.insert(1, "bar");
-            m.insert(2, "baz");
-            m
-        };
-        static ref COUNT = HASHMAP.len();
-    }*/
+fn execute_python_module(param: u64, worker: u64, frame: &Vec<u8>) -> String {
 
     lazy_static! {
 
@@ -30,6 +23,8 @@ fn execute_python_module(param: u64, worker: u64, frame: &Vec<u8>) -> f64 {
 
     // https://www.learnopencv.com/keras-tutorial-using-pre-trained-imagenet-models/
     static ref PYMODULE : &'static pyo3::types::module::PyModule =   PyModule::from_code(*PY, "
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import keras
 import numpy as np
 from keras.applications.imagenet_utils import decode_predictions
@@ -38,51 +33,34 @@ vgg_model = keras.applications.vgg16.VGG16(weights='imagenet')
 print(\"Creation context\")
 
 def main(frame):
-    print('0')
     image = np.frombuffer(frame, dtype=np.uint8)
-    print('0.5')
     image = np.reshape(image, (640, 360, 3))
-    print('1')
     image = image[208:432, 68:292, :]
-    print('1.5', image.shape)
     image_batch = np.expand_dims(image, axis=0)
-    print('2')
     processed_image = keras.applications.vgg16.preprocess_input(image_batch.copy())
-    print('3')
- 
     predictions = vgg_model.predict(processed_image)
-    print('4')
-    label = decode_predictions(predictions)
-    print('5')
-    print(label)
-    print('6')
-    return len(frame)
+    netout = decode_predictions(predictions)
+    result = ''
+    for x in netout[0]:
+        result += x[1] + ':' + str(x[2]) + ':'
+    #labels = {x[1] : x[2] for x in netout[0]}
+    #print(labels)
+    return result
             ", "pymodule.py", "pymodule").map_err(|e| e.print(*PY)).unwrap();
 
     }
-    
-
-    println!("{:?} - We have a python.", worker);
-    
-
-    println!("{:?} - We have a module.", worker);
 
     //let buff = PyBuffer::new(*PY);
     let buff = PyByteArray::new(*PY, frame);
 
     let something = PYMODULE.call1("main", (buff,));
 
-    println!("{:?} - We called it!", worker);
+    let pyresult: String = (*something.map_err(|e| e.print(*PY)).unwrap()).extract().map_err(|e| e.print(*PY)).unwrap();
 
-    let pyresult: f64 = (*something.map_err(|e| e.print(*PY)).unwrap()).extract().map_err(|e| e.print(*PY)).unwrap();
+    //println!("Result: {:?}", pyresult);
+
     return pyresult;
 }
-
-use opencv::prelude::*;
-use opencv::core;
-use opencv::videoio;
-use opencv::highgui;
-use std::ptr;
 
 // https://doc.rust-lang.org/std/ptr/fn.copy.html
 fn from_buf_raw<T>(ptr: *const T, elts: usize) -> Vec<T> {
@@ -95,10 +73,6 @@ fn from_buf_raw<T>(ptr: *const T, elts: usize) -> Vec<T> {
 }
 
 fn main() {
-
-    /*lazy_static! {
-        static ref gil : pyo3::prelude::GILGuard = Python::acquire_gil();
-    }*/
 
     // construct and execute a timely dataflow
     timely::execute_from_args(std::env::args(), |worker| {
@@ -122,6 +96,7 @@ fn main() {
                             }
                         }
                     })
+                    .inspect(move |x: &(u64, u64)| println!("node {:?} pipeline {:?} frame {:?} - pre first", index, (*x).1, (*x).0))
                     .unary(Pipeline, "Frame", |_, _| {
                         let mut vector : Vec<(u64, u64)> = Vec::new();
                         let mut cam = videoio::VideoCapture::new_from_file_with_backend("video-data/policeChase.mp4",videoio::CAP_ANY).unwrap();
@@ -133,7 +108,7 @@ fn main() {
                                 for datum in vector.drain(..) {
                                     cam.read(&mut frame);
                                     let framesize = frame.size().unwrap().width * frame.size().unwrap().height * 3;
-                                    println!("Got frame {}, with width {} and height {}", datum.0, frame.size().unwrap().width, frame.size().unwrap().height);
+                                    //println!("Got frame {}, with width {} and height {}", datum.0, frame.size().unwrap().width, frame.size().unwrap().height);
                                     // let first = frame.at<double>(i,j);
                                     unsafe { session.give((datum.0, datum.1, from_buf_raw(frame.ptr(0).unwrap(), framesize as usize))); }
                                 }
@@ -142,12 +117,17 @@ fn main() {
                     })
                     .exchange(|x: &(u64, u64, Vec<u8>)| (*x).1)
                     //.map(|x| x * 2)
-                    .inspect(move |x: &(u64, u64, Vec<u8>)| println!("node {:?} pipeline {:?} frame {:?} - first", index, (*x).1, (*x).0))
+                    .inspect(move |x: &(u64, u64, Vec<u8>)| println!("node {:?} pipeline {:?} frame {:?} - done first (size {:?})", index, (*x).1, (*x).0, (*x).2.len()))
                     //.exchange(|x: &(u64, u64, Vec<u8>)| (*x).1 + 5)
                     .exchange(|x: &(u64, u64, Vec<u8>)| 5)
+
+                    .map(move |x: (u64, u64, Vec<u8>)| (x.0, x.1, execute_python_module(x.0, index as u64, &(x.2))))
                     //.map(|x| x * 3)
-                    .inspect(move |x: &(u64, u64, Vec<u8>)| println!("node {:?} pipeline {:?} frame {:?} - final ({:?})", 
-                                                            index, (*x).1, (*x).0, execute_python_module((*x).0, index as u64, &(*x).2)));
+                    .inspect(move |x: &(u64, u64, String)| println!("node {:?} pipeline {:?} frame {:?} - done second (result {:?})", index, (*x).1, (*x).0, (*x).2))
+
+                    .map(|x: (u64, u64, String)| (x.0, x.1, x.2.split(":").next().unwrap().to_owned()))
+
+                    .inspect(move |x: &(u64, u64, String)| println!("node {:?} pipeline {:?} frame {:?} - final (choice {:?})", index, (*x).1, (*x).0, (*x).2));
             input
         });
 

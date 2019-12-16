0b1d77504d9720eabd1e432f8cdf091044794c96
Murray Dunne <mrmurraydunne@gmail.com>
2019-12-16 00:45:57 -0500

    stdout err

--- launch_dockers3.sh
+++ launch_dockers3.sh
@@ -2,7 +2,7 @@
 
 
 launch_instance() {
-    docker run --network="host" --gpus all -v /home/ubuntu/cs848project:/home/cs848project calumbruton/timely-experiment:v5 /bin/bash -c "cd /home/cs848project/timely_experiment/; ./target/debug/timely_experiment -w $1 -n $2 --hostfile hosts.txt -p $3" &
+    docker run --network="host" --gpus all -v /home/ubuntu/cs848project:/home/cs848project calumbruton/timely-experiment:v5 /bin/bash -c "cd /home/cs848project/timely_experiment/; ./target/debug/timely_experiment -w $1 -n $2 --hostfile hosts.txt -p $3" 2>&1 &
 }
 
 launch_instance 1 17 4
